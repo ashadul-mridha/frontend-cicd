a07c3b20353c1299321160e997aaf56e43c94da8
Ashadul Islam <ashadulmridhaprog@gmail.com>
2024-09-30 17:49:06 +0600

    modify: add project port

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h3>Deploy using github action</h3>
+      <h4>Project run on: 5173</h4>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
